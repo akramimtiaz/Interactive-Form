6e48e9b43fdfcb87c0156d5c0df7b3c67836de94
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-04-09 14:45:55 +1000

    Added validateName()
    - Added validateName() function which validates the user's name in the format of First Last.
    - Added EventListener on Name Input field.
    - Added CSS Styling to indicate invalid Input

--- js/form.js
+++ js/form.js
@@ -6,6 +6,26 @@
 name regex = /^[A-Za-z]{1}[a-z]+ [A-Za-z]{1}[a-z]+$/
 */
 
+const validateName = () => {
+    const input = $('#name').val();
+    const regex = /^[A-Za-z]{1}[a-z]+ [A-Za-z]{1}[a-z]+$/;
+
+    if(regex.test(input) === true){
+        if($('#name').hasClass('invalid') === true){
+            $('#name').removeClass('invalid');
+        } 
+    } else {
+        if($('#name').hasClass('invalid') === false){
+            $('#name').addClass('invalid');
+        }
+    }
+}
+
+$('#name').attr('autofocus', true);
+
+
 $(document).ready(()=>{
-    console.log('yee');
+    
+    $('#name').on('input', validateName);
+
 });
