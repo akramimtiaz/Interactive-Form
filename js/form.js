306ae5354e3377d2188f53d4947d3ab7edfe4a39
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-04-09 15:07:28 +1000

    Added validateEmail()
    - Added validateEmail() function to validate email address. Regex obtained from online.
    - Added EventListener to Mail Input field.

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@
 
 /*
 name regex = /^[A-Za-z]{1}[a-z]+ [A-Za-z]{1}[a-z]+$/
+email regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 */
 
 const validateName = () => {
@@ -21,11 +22,29 @@ const validateName = () => {
     }
 }
 
+const validateEmail = () => {
+    const input = $('#mail').val();
+    //regex obtained from www.regular-expressions.info
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+    if(regex.test(input) === true){
+        if($('#mail').hasClass('invalid') === true){
+            $('#mail').removeClass('invalid');
+        } 
+    } else {
+        if($('#mail').hasClass('invalid') === false){
+            $('#mail').addClass('invalid');
+        }
+    }
+}
+
 $('#name').attr('autofocus', true);
 
 
 $(document).ready(()=>{
     
     $('#name').on('input', validateName);
+    $('#mail').on('input', validateEmail);
+    
 
 });
