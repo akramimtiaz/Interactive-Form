ab610c303ffc14c49ac79823d4416da74f0db657
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-04-11 09:20:33 +1000

    Submit Functionality Operational

--- js/form.js
+++ js/form.js
@@ -2,20 +2,6 @@
  Team Treehouse - FS JS Techdegree - Project 3 - Interactive Form with Validation
 ***********************************************************************************/
 
-const dateRegex = /\w{6,9} \d{1,2}(am|pm)-\d{1,2}(am|pm)/g;
-
-/*
-name regex = /^[A-Za-z]{1}[a-z]+ [A-Za-z]{1}[a-z]+$/
-email regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-other regex = /^[A-Za-z ]+$/;
-Time regex = /\d{1,2}(am|pm)-\d{1,2}(am|pm)/g;
-            // $('.activities input[type="checkbox"]')[1].parentNode.textContent.match(/\d{1,2}(am|pm)-\d{1,2}(am|pm)/g)[0];
-
-Cost regex = /\$\d{1,}/g;
-or obtain price
-console.log(str.slice(str.indexOf('$')+1));
-*/
-
 const addClassInvalid = ($element) => {
     if($element.hasClass('invalid') === false) $element.addClass('invalid');
 }
@@ -28,37 +14,19 @@ const validateInput = ($element, regex) => {
     const input = $element.val();
     if(regex.test(input) === true){
         removeClassInvalid($element);
-        return true;
     } else {
         addClassInvalid($element);
-        return false;
     }
 }
 
 
 const validateSelectedJob = () => {
-    if($('#title').val() === "select_title"){
-        addClassInvalid( $('#title') );
-        $('#other-title').hide();
-        return false;
-    } else {
-        removeClassInvalid( $('#title') );
-        if($('#title').val() === "other"){
-            $('#other-title').show();
-        } else {
-            $('#other-title').hide();
-        }
-        return true;
-    }
-}
 
-const validateJobRole = () => {
-    const regex = /^[A-Za-z ]+$/;
-    if($('#other-title').is(":visible") === true){
-        return validateInput($('#other-title'), regex);
-    } else { //other-title was not selected, therefore input is N/A
-        return true;
-    }
+    const selectedRole = $('#title').val();
+
+    selectedRole === "select_title" ? addClassInvalid($('#title')) : removeClassInvalid($('#title'));
+    selectedRole === "other" ? $('#other-title').show() : $('#other-title').hide();
+   
 }
 
 const validateSelectedDesign = () => {
@@ -69,7 +37,6 @@ const validateSelectedDesign = () => {
 
         addClassInvalid($('#design'));
         $('#colors-js-puns').hide();
-        return false;
 
     } else {
 
@@ -78,36 +45,36 @@ const validateSelectedDesign = () => {
         $('#color option').attr('selected', false);
 
         if(selectedOption === "js puns"){
-            $('#color option:eq(0)').attr('selected', true);
+            $('#color option[value="cornflowerblue"]').attr('selected', true);
             $('#color option:lt(3)').show();
         } 
         else if (selectedOption === "heart js"){
-            $('#color option:eq(3)').attr('selected', true);
+            $('#color option[value="tomato"]').attr('selected', true);
             $('#color option:gt(2)').show();
         }
 
         $('#colors-js-puns').show();
-        return true;
     }
 
 }
 
-const updateActivityList = (selectedActivity, selectedDate, activitySelected) => {
+const updateActivityList = (selectedActivityName, selectedActivityDate, selectedActivityChecked) => {
 
     $('.activities input[type="checkbox"]').each(function(index){
-        const currentActivity = $(this).parent().text();
+        const $currentActivity = $(this);
+        const currentActivityName = $(this).parent().text();
 
-        if(index !== 0 && currentActivity !== selectedActivity){
-            const currentActivityDate = currentActivity.match(dateRegex)[0];
-           
-            if(currentActivityDate === selectedDate){
+        if(index !== 0 && currentActivityName !== selectedActivityName){
+
+            const currentActivityDate = currentActivityName.match(dateRegex)[0];
+            if(currentActivityDate === selectedActivityDate){
                 
-                if(activitySelected){
-                    $(this).attr('disabled', true);
-                    $(this).parent().addClass('strike');
+                if(selectedActivityChecked){
+                    $currentActivity.attr('disabled', true);
+                    $currentActivity.parent().addClass('strike');
                 } else {
-                    $(this).attr('disabled', false);
-                    $(this).parent().removeClass('strike');
+                    $currentActivity.attr('disabled', false);
+                    $currentActivity.parent().removeClass('strike');
                 }
 
             }
@@ -115,65 +82,59 @@ const updateActivityList = (selectedActivity, selectedDate, activitySelected) =>
     });
 }
 
-const validateSelectedActivity = (activity) => {
+const validateSelectedActivity = ($activity) => {
 
     $(':checkbox:checked').length > 0 ? removeClassInvalid($('.activities legend')) : addClassInvalid($('.activities legend'));
 
-    const activityName = activity.parentNode.textContent;
-    const activityCost = parseInt(activityName.slice(activityName.indexOf('$')+1));
-    const activityDate = activityName.match(dateRegex) ? activityName.match(dateRegex)[0] : null;
+    if($activity !== null){
+        const activityName = $activity.parent().text();
+        const activityCost = parseInt(activityName.slice(activityName.indexOf('$')+1));
+        const activityDate = activityName.match(dateRegex) ? activityName.match(dateRegex)[0] : null;
 
-    let   currentTotal = parseInt($('#total-cost').text().slice(1));
-    activity.checked ? currentTotal += activityCost : currentTotal -= activityCost;
-    $('#total-cost').text('$'+currentTotal);
+        updateActivityList(activityName, activityDate, $activity.is(':checked'));
 
-    updateActivityList(activityName, activityDate, activity.checked);
+        let currentTotal = parseInt($('#total-cost').text());
+        $activity.is(':checked') ? currentTotal += activityCost : currentTotal -= activityCost;
+        $('#total-cost').text(currentTotal); 
+    }
 }
 
 const validatePaymentMethod = () => {
 
     const paymentMethod = $('#payment').val();
+    paymentMethod === "select_method" ? addClassInvalid($('#payment')) :  removeClassInvalid($('#payment'));
     $('fieldset:last > div').hide();
 
     if(paymentMethod === "credit card"){
-        removeClassInvalid($('#payment'));
         $('fieldset:last > div:eq(0)').show();
 
-
     } else if (paymentMethod === "paypal"){
-        removeClassInvalid($('#payment'));
         $('fieldset:last > div:eq(1)').show();
 
-
     } else if (paymentMethod === "bitcoin"){
-        removeClassInvalid($('#payment'));
         $('fieldset:last > div:eq(2)').show();
 
     } else { //paymentMethod === "select_method"
-        addClassInvalid($('#payment'));
         $('fieldset:last > div').hide();
-        return false;
     }
 }
 
 const valCCInfo = (field) => {
-    const cardRegex = /^\d{13,16}$/;
+    const creditCardRegex = /^\d{13,16}$/;
     const zipRegex = /^\d{5}$/;
     const cvvRegex = /^\d{3}$/;
 
-    if($('#credit-card').is(":visible") === true){
-        if(field === "cc-num"){
-            return validateInput($('#cc-num'), cardRegex);
-        } else if (field === "zip"){
-            return validateInput($('#zip'), zipRegex);
-        } else if (field === "cvv"){
-            return validateInput($('#cvv'), cvvRegex);
-        }
-    } else {
-        return true;
+    if(field === "cc-num"){
+        return validateInput($('#cc-num'), creditCardRegex);
+    } else if (field === "zip"){
+        return validateInput($('#zip'), zipRegex);
+    } else if (field === "cvv"){
+        return validateInput($('#cvv'), cvvRegex);
     }
+    
 }
 
+const dateRegex = /\w{6,9} \d{1,2}(am|pm)-\d{1,2}(am|pm)/g;
 
 $('#name').attr('autofocus', true);
 
@@ -184,7 +145,7 @@ $(document).ready(()=>{
 
     //email regex obtained from www.regular-expressions.info
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    const nameRegex = /^[A-Za-z]{1}[a-z]+ [A-Za-z]{1}[a-z]+$/;
+    const nameRegex = /^[A-Za-z]{1}[a-z]* [A-Za-z]{1}[a-z]*$/;
     
     const otherJobRegex = /^[A-Za-z ]+$/;
     const creditCardRegex =  /^\d{13,16}$/;
@@ -199,22 +160,20 @@ $(document).ready(()=>{
 
     //Element used to track Total Cost
     $('<span>').attr('id','total-cost')
-            .text('$0')
-            .appendTo($('.activities'));
+               .text('0')
+               .appendTo($('.activities'));
 
     //Set Credit-Card as default Payment Option and hide Paypal and Bitcoin Payment options.
     $('#payment').val("credit card");
     $('#credit-card ~ div').hide();
 
-
-   
     //Event Listener for Name Input field
     $('#name').on('input blur', ()=>{
         validateInput($('#name'), nameRegex);
     });
 
     //Event Listener for Email Input field
-    $('#mail').on('input blur', ()=>{
+    $('#mail').on('input blur', (e)=>{
         validateInput($('#mail'), emailRegex);
     });
 
@@ -235,42 +194,45 @@ $(document).ready(()=>{
 
     //Event Listener for Checkboxes attached to Fieldset
     $('.activities').on('change', (e) => {
-        validateSelectedActivity(e.target);
+        validateSelectedActivity($(e.target));
     });
 
     //Event Listener for Credit Card Input fields attached to Div
-    $('#credit-card').on('input', (e) => {
+    $('#credit-card').on('input blur', (e) => {
         valCCInfo(e.target.id);
     });
 
     //Event Listner for when Form is Submitted
     $('form').on('submit', (e) => {
         
-        formValid = validateInput($('#name'), nameRegex);
-        formValid = validateInput($('#mail'), emailRegex);
-        formValid = validateSelectedJob();
+        
+        validateInput($('#name'), nameRegex);
+        validateInput($('#mail'), emailRegex);
+        validateSelectedJob();
+        validateSelectedDesign();
+        validateSelectedActivity(null);
+        validatePaymentMethod();
+
         if($('#other-title').is(":visible")){
-            formValid = validateInput($('#other-title'), otherJobRegex);
-        }
-        
-
-        if($(':checkbox:checked').length === 0){
-            $('.activities legend').addClass('invalid');
-            formValid = false;
+            validateInput($('#other-title'), otherJobRegex);
+        }else{
+            removeClassInvalid($('#other-title'));
         }
 
-        formValid = validateSelectedDesign();
-        
-        
-
-        console.log(formValid);
-        
-        if(!formValid){
-            e.preventDefault();
+        if($('#payment').val() === "credit card"){
+            validateInput($('#cc-num'), creditCardRegex);
+            validateInput($('#zip'), zipRegex);
+            validateInput($('#cvv'), cvvRegex);
         } else {
-            e.preventDefault();
-            alert('Ahlan wa Sahlan bik - Ey!');
+            removeClassInvalid($('#credit-card input'));
         }
+
+        if($('.invalid').length === 0){
+            alert('Form has been successfully submitted!');
+        }else{
+            e.preventDefault();
+        }
+        
 
     });
 
