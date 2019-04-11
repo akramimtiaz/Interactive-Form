938abd4287fb20f8a459ccd99ca2191a4c93dad9
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-04-11 12:57:32 +1000

    Added Comments

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@
  Team Treehouse - FS JS Techdegree - Project 3 - Interactive Form with Validation
 ***********************************************************************************/
 
+const dateRegex = /\w{6,9} \d{1,2}(am|pm)-\d{1,2}(am|pm)/g;
+
 const addClassInvalid = ($element) => {
     $element.removeClass('valid');
     $element.addClass('invalid');
@@ -25,28 +27,34 @@ const validateInput = ($element, regex) => {
 
 const validateSelectedJob = () => {
 
+    //Retrive the selected option from the Job Role Drop-down
     const selectedRole = $('#title').val();
 
     selectedRole === "select_title" ? addClassInvalid($('#title')) : addClassValid($('#title'));
+    //Only show the "Other" Input field if the "Other" option was selected
     selectedRole === "other" ? $('#other-title').show() : $('#other-title').hide();
    
 }
 
 const validateSelectedDesign = () => {
 
+    //Retrive the selected option from the Design Drop-down
     const selectedOption = $('#design').val();
 
     if(selectedOption === "Select Theme"){
 
         addClassInvalid($('#design'));
+        //Hide the "Colors" drop-down if the selected option was "Select Theme"
         $('#colors-js-puns').hide();
 
     } else {
 
         addClassValid($('#design'));
+        //Hide and Deselect all options within the "Colors" Drop-down
         $('#color option').hide();
         $('#color option').attr('selected', false);
 
+        //Show the appropriate options based on the "Design" choosen.
         if(selectedOption === "js puns"){
             $('#color option[value="cornflowerblue"]').attr('selected', true);
             $('#color option:lt(3)').show();
@@ -55,27 +63,30 @@ const validateSelectedDesign = () => {
             $('#color option[value="tomato"]').attr('selected', true);
             $('#color option:gt(2)').show();
         }
-
+        //Show "Colors" Drop-down
         $('#colors-js-puns').show();
     }
 
 }
 
 const updateActivityList = (selectedActivityName, selectedActivityDate, selectedActivityChecked) => {
-
+    //Loop through each Activity checkbox within Activities
     $('.activities input[type="checkbox"]').each(function(index){
-        const $currentActivity = $(this);
+        //current checkbox being examined
+        const $currentActivity = $(this); 
+        //obtain Activity name from label element
         const currentActivityName = $(this).parent().text();
 
+        //Avoid "Main Conference" and the Selected Activity itself 
         if(index !== 0 && currentActivityName !== selectedActivityName){
-
+            //Extract Date and Time of Activity
             const currentActivityDate = currentActivityName.match(dateRegex)[0];
             if(currentActivityDate === selectedActivityDate){
-                
+                //If the Selected Activity was checked, Disable the conflicting activity
                 if(selectedActivityChecked){
                     $currentActivity.attr('disabled', true);
                     $currentActivity.parent().addClass('strike');
-                } else {
+                } else { //If the Selected Activity was unchecked, Re-enable the conflicting activity
                     $currentActivity.attr('disabled', false);
                     $currentActivity.parent().removeClass('strike');
                 }
@@ -87,15 +98,19 @@ const updateActivityList = (selectedActivityName, selectedActivityDate, selected
 
 const validateSelectedActivity = ($activity) => {
 
+    //If no checkboxes are checked, change the text color of 'Register for Activities'
     $(':checkbox:checked').length === 0 ? addClassInvalid($('.activities legend')) : addClassValid($('.activities legend'));
-
+    //This if statement is here so this function can be called outside of the event listener
     if($activity !== null){
+        //Obtain the various information activity
         const activityName = $activity.parent().text();
         const activityCost = parseInt(activityName.slice(activityName.indexOf('$')+1));
         const activityDate = activityName.match(dateRegex) ? activityName.match(dateRegex)[0] : null;
 
+        //Update the Activities list to reflect any clashing events
         updateActivityList(activityName, activityDate, $activity.is(':checked'));
 
+        //Update the total cost of the event
         let currentTotal = parseInt($('#total-cost').text());
         $activity.is(':checked') ? currentTotal += activityCost : currentTotal -= activityCost;
         $('#total-cost').text(currentTotal); 
@@ -103,9 +118,10 @@ const validateSelectedActivity = ($activity) => {
 }
 
 const validatePaymentMethod = () => {
-
+    //Retrive the selected option from the Payment Drop-down
     const paymentMethod = $('#payment').val();
     paymentMethod === "select_method" ? addClassInvalid($('#payment')) :  addClassValid($('#payment'));
+    //Hide all options within the Payment section
     $('fieldset:last > div').hide();
 
     if(paymentMethod === "credit card"){
@@ -127,7 +143,7 @@ const validateCreditCard = (field) => {
     if(field === "cc-num"){
         validateInput($('#cc-num'), creditCardRegex);
         if($('#cc-num').hasClass('invalid') && $('#cc-num').val() === ""){
-            
+
         }
         
     } else if (field === "zip"){
@@ -138,7 +154,7 @@ const validateCreditCard = (field) => {
     
 }
 
-const dateRegex = /\w{6,9} \d{1,2}(am|pm)-\d{1,2}(am|pm)/g;
+
 
 $('#name').attr('autofocus', true);
 
