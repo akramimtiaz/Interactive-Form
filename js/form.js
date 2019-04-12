62b92054a587b722116baba0574488667a648b4e
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-04-12 19:45:18 +1000

    Full Functionality Achieved, Final Comments Added

--- js/form.js
+++ js/form.js
@@ -2,8 +2,21 @@
  Team Treehouse - FS JS Techdegree - Project 3 - Interactive Form with Validation
 ***********************************************************************************/
 
+//email regex obtained from www.regular-expressions.info
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+const nameRegex = /^[A-Za-z]{1}[a-z]* [A-Za-z]{1}[a-z]*$/;
 const dateRegex = /\w{6,9} \d{1,2}(am|pm)-\d{1,2}(am|pm)/g;
-const emptyFieldErrorMessage = "Empty Field";
+
+const otherJobRegex = /^[A-Za-z ]+$/;
+const creditCardRegex =  /^\d{13,16}$/;
+const zipRegex = /^\d{5}$/;
+const cvvRegex =  /^\d{3}$/;
+
+//error messages for the respective input fields
+const nameErrorMsg = "Please enter a valid name";
+const emailErrorMsg = "Please enter a valid email";
+const otherJobErrorMsg = "Please enter a valid role";
+
 const creditCardErrorMsg = "13-16 Digits";
 const zipErrorMsg = "5 Digits";
 const cvvErrorMsg = "3 Digits";
@@ -18,43 +31,45 @@ const addClassValid = ($element) => {
     $element.removeClass('invalid');
 }
 
-/*Adds/Removes Invalid Class from specified Element if it meets regex requirements*/
-// const validateInput = ($element, regex) => {
-//     const input = $element.val();
-//     if(regex.test(input) === true){
-//         addClassValid($element);
-//     } else {
-//         addClassInvalid($element);
-//     }
-// }
-
-
-const validateInput = ($element, regex, formatErrorMessage) => {
+/*Creates a span element and attaches it to an input's associated label*/
+const createErrorMessage = ($element) => {
+    //obtain input element's associated label 
+    const $elementLabel = $(`label[for="${$element.attr('id')}"]`);
+    //create error message with a class of error
+    const $errorMsg = $('<span>').addClass('error')
     
-    const input = $element.val();
-
+    //remove any existing error messages associated with the current input field
     $(`label[for="${$element.attr('id')}"] > span`).remove();
-    const $errorMsg = $('<span>').addClass('error').hide();
+    //append the newly created error message to the respective label
+    $errorMsg.appendTo($elementLabel);
+    
+    return $errorMsg;
+}
 
-    $errorMsg.appendTo($(`label[for="${$element.attr('id')}"]`));
+/*Adds/Removes Invalid Class from specified Element if it meets regex requirements*/
+const validateInput = ($element, regex, errorMessage) => {
+    //obtain user input from respective input field
+    const input = $element.val();
+    //generate an error message for the respective input field
+    const $errorMsg = createErrorMessage($element);
 
     if(regex.test(input) === true){
         addClassValid($element);
         $errorMsg.hide();
     } else {
         addClassInvalid($element);
-        $errorMsg.show();
+        //If the field is blank show the "Empty Field" message
         if(input === ""){
-            $errorMsg.text(emptyFieldErrorMessage);
-        } else {
-            $errorMsg.text(formatErrorMessage);
+            $errorMsg.text("Empty Field");
+        } else { //Else show the error message specific to the input field
+            $errorMsg.text(errorMessage);
         }
+        $errorMsg.show();
     }
-    
+
 }
 
-
-
+/*Validate the Job Role selected by the user*/
 const validateSelectedJob = () => {
 
     //Retrive the selected option from the Job Role Drop-down
@@ -66,6 +81,7 @@ const validateSelectedJob = () => {
    
 }
 
+/*Validate the T-Shirt Design selected by the user*/
 const validateSelectedDesign = () => {
 
     //Retrive the selected option from the Design Drop-down
@@ -99,6 +115,7 @@ const validateSelectedDesign = () => {
 
 }
 
+/*Updates the Activities list to reflect any clashing events*/
 const updateActivityList = (selectedActivityName, selectedActivityDate, selectedActivityChecked) => {
     //Loop through each Activity checkbox within Activities
     $('.activities input[type="checkbox"]').each(function(index){
@@ -126,27 +143,29 @@ const updateActivityList = (selectedActivityName, selectedActivityDate, selected
     });
 }
 
+/*Validates the activites selected by the user*/
 const validateSelectedActivity = ($activity) => {
 
     //If no checkboxes are checked, change the text color of 'Register for Activities'
     $(':checkbox:checked').length === 0 ? addClassInvalid($('.activities legend')) : addClassValid($('.activities legend'));
     //This if statement is here so this function can be called outside of the event listener
-    if($activity !== null){
-        //Obtain the various information activity
-        const activityName = $activity.parent().text();
-        const activityCost = parseInt(activityName.slice(activityName.indexOf('$')+1));
-        const activityDate = activityName.match(dateRegex) ? activityName.match(dateRegex)[0] : null;
+    
+    //Obtain the various information activity
+    const activityName = $activity.parent().text();
+    const activityCost = parseInt(activityName.slice(activityName.indexOf('$')+1));
+    const activityDate = activityName.match(dateRegex) ? activityName.match(dateRegex)[0] : null;
 
-        //Update the Activities list to reflect any clashing events
-        updateActivityList(activityName, activityDate, $activity.is(':checked'));
+    //Update the Activities list to reflect any clashing events
+    updateActivityList(activityName, activityDate, $activity.is(':checked'));
 
-        //Update the total cost of the event
-        let currentTotal = parseInt($('#total-cost').text());
-        $activity.is(':checked') ? currentTotal += activityCost : currentTotal -= activityCost;
-        $('#total-cost').text(currentTotal); 
-    }
+    //Update the total cost of the event
+    let currentTotal = parseInt($('#total-cost').text());
+    $activity.is(':checked') ? currentTotal += activityCost : currentTotal -= activityCost;
+    $('#total-cost').text(currentTotal); 
+    
 }
 
+/*Validates the payment method selected by the user*/
 const validatePaymentMethod = () => {
     //Retrive the selected option from the Payment Drop-down
     const paymentMethod = $('#payment').val();
@@ -154,6 +173,7 @@ const validatePaymentMethod = () => {
     //Hide all options within the Payment section
     $('fieldset:last > div').hide();
 
+    //Only show the information relevant to the selected payment option
     if(paymentMethod === "credit card"){
         $('fieldset:last > div:eq(0)').show();
 
@@ -165,10 +185,8 @@ const validatePaymentMethod = () => {
     }
 }
 
+/*Validates a given credit-card field*/
 const validateCreditCard = (field) => {
-    const creditCardRegex = /^\d{13,16}$/;
-    const zipRegex = /^\d{5}$/;
-    const cvvRegex = /^\d{3}$/;
 
     if(field === "cc-num"){
         validateInput($('#cc-num'), creditCardRegex, creditCardErrorMsg);
@@ -181,26 +199,11 @@ const validateCreditCard = (field) => {
 }
 
 
-
+//Set focus to the Name Input field when the page loads
 $('#name').attr('autofocus', true);
 
 
 $(document).ready(()=>{
-
-    //email regex obtained from www.regular-expressions.info
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    const nameRegex = /^[A-Za-z]{1}[a-z]* [A-Za-z]{1}[a-z]*$/;
-    
-    const otherJobRegex = /^[A-Za-z ]+$/;
-    const creditCardRegex =  /^\d{13,16}$/;
-    const zipRegex = /^\d{5}$/;
-    const cvvRegex =  /^\d{3}$/;
-
-    //error messages for the respective input fields
-    const nameErrorMsg = "Please enter a valid name";
-    const emailErrorMsg = "Please enter a valid email";
-    const otherJobErrorMsg = "Please enter a valid role";
-    
 
 
     //hide "Other" input field
@@ -255,22 +258,27 @@ $(document).ready(()=>{
 
     //Event Listner for when Form is Submitted
     $('form').on('submit', (e) => {
-        
+
+        //prevent the page from refreshing
+        e.preventDefault();
+        //validate various form fields
         validateInput($('#name'), nameRegex, nameErrorMsg);
         validateInput($('#mail'), emailRegex, emailErrorMsg);
         validateSelectedJob();
         validateSelectedDesign();
-        /*MODIFY THIS LINE TO JUST THE IF STATMEMENT
-        REMOVE THE IF NULL FROM THE FUNCTION */
-        validateSelectedActivity(null);
         validatePaymentMethod();
 
+        //Checks if no checkboxes are checked, if so adds the invalid class
+        $(':checkbox:checked').length === 0 ? addClassInvalid($('.activities legend')) : addClassValid($('.activities legend'));
+
+        //Validates the 'Other Job Role' field if "Other" was selected from the Job Role drop-down
         if($('#other-title').is(":visible")){
             validateInput($('#other-title'), otherJobRegex, otherJobErrorMsg);
         }else{
             addClassValid($('#other-title'));
         }
 
+        //Validates the Credit Card fields if the selected payment option is "Credit Card"
         if($('#payment').val() === "credit card"){
             validateInput($('#cc-num'), creditCardRegex, creditCardErrorMsg);
             validateInput($('#zip'), zipRegex, zipErrorMsg);
@@ -279,25 +287,28 @@ $(document).ready(()=>{
             addClassValid($('#credit-card input'));
         }
 
+        //If there are no fields that have not been filled
         if($('.invalid').length === 0){
-            e.preventDefault();
+            //scroll to the top of the page
             window.scrollTo(0, 0);
-            
+            //generate successfully submitted message
             $('<p>').attr('id', 'submit-success')
                     .text('Your form has been successfully submitted!')
                     .appendTo($('.container header'))
-                    .delay(2000).slideUp(500)
+                    .delay(2000).slideUp(500);
+            //reload the page after showing the message
             setTimeout(()=>{location.reload();}, 2500);
-        }else{
-            e.preventDefault();
+
+        } else {
+            //scroll to the top of the page
             window.scrollTo(0, 0);
+            //generate an error message
             $('<p>').attr('id', 'submit-failed')
             .text('Please ensure all fields have been filled')
             .appendTo($('.container header'))
             .delay(2000).slideUp(500)
         }
         
-
     });
 
 });
